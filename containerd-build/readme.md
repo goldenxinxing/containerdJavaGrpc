878e9382015e3f07377a32b47e8f94b13016d35b
goldenxinxing <15031259256@163.com>
2022-03-19 22:01:22 +0800

    add something

--- containerd-build/readme.md
+++ containerd-build/readme.md
@@ -1,1 +1,2 @@
 https://medium.com/@Mark.io/how-to-communicate-with-containerd-using-java-f37d349b9ead
+mvn -N io.takari:maven:0.7.6:wrapper
---
+++ java-image/src/main/java/hello/Application.java
@@ -0,0 +1,29 @@
+package hello;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@SpringBootApplication
+@RestController
+public class Application {
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/env/{name}")
+    public String env(@PathVariable("name") String name) {
+        log.info("env:{},value is:{}", name, environment.getProperty(name));
+        return String.format("env:%s,value is:%s",  name, environment.getProperty(name));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
