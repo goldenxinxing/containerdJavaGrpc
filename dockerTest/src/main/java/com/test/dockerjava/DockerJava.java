386548de423148f41fde5f3f1f8025dbdbf42071
goldenxinxing <15031259256@163.com>
2022-03-31 18:27:29 +0800

    systemInfo

--- dockerTest/src/main/java/com/test/dockerjava/DockerJava.java
+++ dockerTest/src/main/java/com/test/dockerjava/DockerJava.java
@@ -114,6 +114,32 @@ public class DockerJava {
         }
     }
 
+    public static void main(String[] args) {
+        SystemInfo systemInfo = new SystemInfo();
+        CentralProcessor processor = systemInfo.getHardware().getProcessor();
+        long[] prevTicks = processor.getSystemCpuLoadTicks();
+        long[] ticks = processor.getSystemCpuLoadTicks();
+        long nice = ticks[CentralProcessor.TickType.NICE.getIndex()]
+            - prevTicks[CentralProcessor.TickType.NICE.getIndex()];
+        long irq = ticks[CentralProcessor.TickType.IRQ.getIndex()]
+            - prevTicks[CentralProcessor.TickType.IRQ.getIndex()];
+        long softirq = ticks[CentralProcessor.TickType.SOFTIRQ.getIndex()]
+            - prevTicks[CentralProcessor.TickType.SOFTIRQ.getIndex()];
+        long steal = ticks[CentralProcessor.TickType.STEAL.getIndex()]
+            - prevTicks[CentralProcessor.TickType.STEAL.getIndex()];
+        long cSys = ticks[CentralProcessor.TickType.SYSTEM.getIndex()]
+            - prevTicks[CentralProcessor.TickType.SYSTEM.getIndex()];
+        long user = ticks[CentralProcessor.TickType.USER.getIndex()]
+            - prevTicks[CentralProcessor.TickType.USER.getIndex()];
+        long iowait = ticks[CentralProcessor.TickType.IOWAIT.getIndex()]
+            - prevTicks[CentralProcessor.TickType.IOWAIT.getIndex()];
+        long idle = ticks[CentralProcessor.TickType.IDLE.getIndex()]
+            - prevTicks[CentralProcessor.TickType.IDLE.getIndex()];
+        long totalCpu = user + nice + cSys + idle + iowait + irq + softirq + steal;
+        log.info("idle总数= {}", idle);
+        log.info("CPU总数 = {},CPU利用率 ={}", processor.getLogicalProcessorCount(),
+            new DecimalFormat("#.##%").format(1.0 - (idle * 1.0 / totalCpu)));
+    }
     @GetMapping("systemMemInfo")
     public void systemMemInfo() throws InterruptedException {
         for (int i = 10; i > 0; i--) {
